d9d8e4884dc54f338c6d3991113d6a9c7c25f110
gitugiri2k <gitugiri2k@gmail.com>
2023-07-05 02:18:20 -0500

    Initial Setup

---
+++ ClubMembershipApplication/FieldValidators/UserRegistrationValidator.cs
@@ -0,0 +1,75 @@
+﻿using ClubMembershipApplication.Data;
+using FieldValidatorAPI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+namespace ClubMembershipApplication.FieldValidators
+{
+    public class UserRegistrationValidator:IFieldValidator
+	{
+        const int FirstName_Min_Length = 2;
+        const int FirstName_Max_Length = 100;
+        const int LastName_Min_Length = 2;
+        const int LastName_Max_Length = 100;
+        delegate bool EmailExistsDel(string emailAddress);
+        FieldValidatorDel _fieldValidatorDel = null;
+
+        string[] _fieldArray = null;
+
+        public string[] FieldArray
+        {
+            get
+            {
+                if (_fieldArray == null)
+                    _fieldArray = new string[Enum.GetValues(typeof(FieldConstants.UserRegistrationField)).Length];
+                return _fieldArray;
+            }
+        }
+        public FieldValidatorDel ValidatorDel => _fieldValidatorDel;
+
+        public UserRegistrationValidator(IRegister register)
+        {
+            _register = register;
+        }
+
+        public void InitialiseValidatorDelegates()
+        {
+
+        }
+
+        private bool ValidField(int fieldIndex, string fieldValue, string[] fieldArray, out string fieldInvalidMessage)
+        {
+            fieldInvalidMessage = "";
+            FieldConstants.UserRegistrationField userRegistrationField = (FieldConstants.UserRegistrationField)fieldIndex;
+            switch (userRegistrationField)
+            {
+                case FieldConstants.UserRegistrationField.EmailAddress:
+                    break;
+                case FieldConstants.UserRegistrationField.FirstName:
+                    break;
+                case FieldConstants.UserRegistrationField.LastName:
+                    break;
+                case FieldConstants.UserRegistrationField.Password:
+                    break;
+                case FieldConstants.UserRegistrationField.PasswordCompare:
+                    break;
+                case FieldConstants.UserRegistrationField.DateOfBirth:
+                    break;
+                case FieldConstants.UserRegistrationField.PhoneNumber:
+                    break;
+                case FieldConstants.UserRegistrationField.AddressFirstLine:
+                    break;
+                case FieldConstants.UserRegistrationField.AddressSecondLine:
+                    break;
+                case FieldConstants.UserRegistrationField.AddressCity:
+                    break;
+                case FieldConstants.UserRegistrationField.PostCode:
+                    break;
+                default:
+                    throw new ArgumentException("This field does not exists");
+            }
+            return (fieldInvalidMessage == "");
+        }
+    }
+}
+
--- FieldValidationAPI/CommonFieldValidatorFunctions.cs
+++ FieldValidationAPI/CommonFieldValidatorFunctions.cs
@@ -1,52 +1,66 @@
 ﻿using System.Text.RegularExpressions;
 
-namespace FieldValidationAPI;
-
-public delegate bool RequiredValidDel(string fieldVal);
-public delegate bool StringLengthValidDel(string fieldVal, int min, int max);
-public delegate bool DateValidDel(string fieldVal, out DateTime ValidDateTime);
-public delegate bool PatternMatchValidDel(string fieldVal, string pattern);
-public delegate bool CompareFieldValidDel(string fieldVal, string fieldValCompare);
-
-
-public class CommonFieldValidatorFunctions
+namespace FieldValidationAPI
 {
-    private static RequiredValidDel _requiredValidDel = null;
-    private static StringLengthValidDel _stringLengthValidDel = null;
-    private static DateValidDel _dateValidDel = null;
-    private static PatternMatchValidDel _patternMatchValidDel = null;
-    private static CompareFieldValidDel _compareFieldValidDel = null;
+    public delegate bool RequiredValidDel(string fieldVal);
+    public delegate bool StringLengthValidDel(string fieldVal, int min, int max);
+    public delegate bool DateValidDel(string fieldVal, out DateTime ValidDateTime);
+    public delegate bool PatternMatchValidDel(string fieldVal, string pattern);
+    public delegate bool CompareFieldValidDel(string fieldVal, string fieldValCompare);
 
-    private static bool RequiredValidDel(string fieldVal)
+
+    public class CommonFieldValidatorFunctions
     {
-        if (!string.IsNullOrEmpty(fieldVal))
-            return true;
-        return false;
+        private static RequiredValidDel _requiredValidDel = null;
+        private static StringLengthValidDel _stringLengthValidDel = null;
+        private static DateValidDel _dateValidDel = null;
+        private static PatternMatchValidDel _patternMatchValidDel = null;
+        private static CompareFieldValidDel _compareFieldValidDel = null;
+
+        public static RequiredValidDel RequiredFieldValidDel
+        {
+            get
+            {
+                if (_requiredValidDel == null)
+                    _requiredValidDel = new RequiredValidDel(RequiredValid);
+
+                return _requiredValidDel;
+            }
+        }
+
+        private static bool RequiredValid(string fieldVal)
+        {
+            if (!string.IsNullOrEmpty(fieldVal))
+                return true;
+            return false;
+        }
+        private static bool StringLengthValid(string fieldVal, int min, int max)
+        {
+            if (fieldVal.Length >= min && fieldVal.Length <= max)
+                return true;
+            return false;
+        }
+        private static bool DateValid(string fieldVal, out DateTime ValidDateTime)
+        {
+            if (DateTime.TryParse(fieldVal, out ValidDateTime))
+                return true;
+            return false;
+        }
+        private static bool PatternMatchValid(string fieldVal, string regExPattern)
+        {
+            Regex regex = new Regex(regExPattern);
+            if (regex.IsMatch(fieldVal))
+                return true;
+            return false;
+        }
+        private static bool CompareFieldValid(string fieldVal1, string fieldVal2)
+        {
+            if (fieldVal1.Equals(fieldVal2))
+                return true;
+            return false;
+        }
     }
-    private static bool StringLengthValidDel(string fieldVal, int min, int max)
-    {
-        if (fieldVal.Length >= min && fieldVal.Length <= max)
-            return true;
-        return false;
-    }
-    private static bool DateValidDel(string fieldVal, out DateTime ValidDateTime)
-    {
-        if (DateTime.TryParse(fieldVal,out ValidDateTime))
-            return true;
-        return false;
-    }
-    private static bool PatternMatchValidDel(string fieldVal, string regExPattern)
-    {
-        Regex regex = new Regex(regExPattern);
-        if (regex.IsMatch(fieldVal))
-            return true;
-        return false;
-    }
-    private static bool CompareFieldValidDel(string fieldVal1, string fieldVal2)
-    {
-        if (fieldVal1.Equals(fieldVal2))
-            return true;
-        return false;
-    }
+
 }
+
 
